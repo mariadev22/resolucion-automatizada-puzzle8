ce62fb18766eee383f8212e8a44e8721b1cb351d
María Martínez <mariamartinezdev22@gmail.com>
2025-05-28 05:53:46 -0400

    eat(ui): agregar funciones draw_ui y animate_solution para visualizar estados del puzzle
    -se añadió draw_ui() para mostrar el tablero inicial y
    -se añadio animate_solution() para animar el camino desde el estado inicial hasta la meta.
    -estas funciones ayudan a visualizar tanto soluciones como tableros sin solución.

--- ui/display.py
+++ ui/display.py
@@ -2,8 +2,10 @@ import pygame
 
 TILE_SIZE = 100
 MARGIN = 5
+WIDTH = HEIGHT = TILE_SIZE * 3 + MARGIN * 4
 FONT_SIZE = 48
-BOARD_GAP = 50  # Espacio entre tableros
+LABEL_FONT_SIZE = 28
+METRIC_FONT_SIZE = 24
 
 def draw_board(screen, board, offset_x=0, offset_y=0):
     font = pygame.font.SysFont(None, FONT_SIZE)
@@ -23,8 +25,44 @@ def draw_board(screen, board, offset_x=0, offset_y=0):
                 text_rect = text.get_rect(center=rect.center)
                 screen.blit(text, text_rect)
 
-def draw_ui(screen, board1, board2):
-    screen.fill((30, 30, 30))
-    draw_board(screen, board1, offset_x=50, offset_y=50)
-    draw_board(screen, board2, offset_x=400, offset_y=50)
+def draw_label(screen, label):
+    label_font = pygame.font.SysFont(None, LABEL_FONT_SIZE)
+
+    title_label = label_font.render(f"{label}", True, (255, 255, 255))
+    screen.blit(title_label, (10, 20))
+
+def draw_metrics(screen, _nodes, _len, _time):
+    metric_font = pygame.font.SysFont(None, METRIC_FONT_SIZE)
+
+    metrics = [
+        f"Nodos: {_nodes}",
+        f"Solución: {_len}",
+        f"Tiempo: {_time:.6f}s"
+    ]
+    for i, text in enumerate(metrics):
+        metric = metric_font.render(text, True, (255, 255, 255))
+        screen.blit(metric, (10, 370 + i * 20))
+
+def draw_ui(label, screen, board, _nodes, _len, _time):
+    draw_label(screen, label)
+    draw_metrics(screen, _nodes, _len, _time)
+    draw_board(screen, board, offset_x=10, offset_y=50)
     pygame.display.flip()
+
+def animate_solution(label, screen, solution, _nodes, _len, _time):
+    delay=500
+
+    for state in solution:
+        screen.fill((30, 30, 30))
+        draw_label(screen, label + " Resolviendo...")
+        draw_board(screen, state.board, offset_x=10, offset_y=50)
+        pygame.event.pump()
+        pygame.display.flip()
+        pygame.time.delay(delay)
+
+    screen.fill((30, 30, 30))  
+    draw_label(screen, label)
+    draw_board(screen, solution[-1].board, offset_x=10, offset_y=50)
+    draw_metrics(screen, _nodes, _len, _time)
+    pygame.display.flip()
+    
